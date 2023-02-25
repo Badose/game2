b62eba1ab97a259ab5dec70a845c34b50f553654
Lera Konoplyova <vallerytompkens@gmail.com>
2023-02-25 15:58:03 +0300

    animation

--- game2/gameee.py
+++ game2/gameee.py
@@ -17,7 +17,7 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
             running = False
-    player.move()
+    player.update()
     screen.fill((100, 100, 100))
     all_sprites.draw(screen)
     clock.tick(FPS)
--- game2/player.py
+++ game2/player.py
@@ -14,9 +14,18 @@ class Player(pg.sprite.Sprite):
         self.image = self.walk_r[0]
         self.rect = self.image.get_rect()
         self.rect.center = pos
-
-    def move(self):
+        self.frame = 0
+        self.cycle_len = 4
+        self.last_update = 0
         self.velocity = Vector2(0, 0)
+        self.animation_cycle = None
+
+    def update(self):
+        self._move()
+        self._animate() 
+
+    def _move(self):
+        self.velocity.update(0, 0)
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
             self.velocity.y = -1
@@ -45,3 +54,21 @@ class Player(pg.sprite.Sprite):
             self.walk_l.append(sheet.get_image(i, h, w, h))
             self.walk_u.append(sheet.get_image(i, h*2, w, h))
             self.walk_d.append(sheet.get_image(i, h*3, w, h))
+    
+    def _animate(self, frame_len=100):
+        now = pg.time.get_ticks()
+        if now - self.last_update > frame_len and self.velocity.length() > 0:
+            self.last_update = now
+        if self.velocity.y < 0:
+            self.animation_cycle = self.walk_u
+        elif self.velocity.y > 0:
+            self.animation_cycle = self.walk_d
+        elif self.velocity.x > 0:
+            self.animation_cycle = self.walk_r
+        elif self.velocity.x < 0:
+            self.animation_cycle = self.walk_l
+
+        self.frame = (self.frame + 1) % self.cycle_len
+        self.image = self.animation_cycle[self.frame]
+
+        
